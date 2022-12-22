f3e9ab91d76795174ac1eac8a1af2dcad85f8ed1
Pavel Mikhaylenko <104693653+westteern@users.noreply.github.com>
2022-12-22 18:08:21 +0200

    add fetch trends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,10 @@
+import { fetchTrendingMovies } from 'api-service/apiService';
+import { useEffect } from 'react';
+
 export const App = () => {
+  useEffect(() => {
+    fetchTrendingMovies();
+  });
   return (
     <div
       style={{
@@ -7,7 +13,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
